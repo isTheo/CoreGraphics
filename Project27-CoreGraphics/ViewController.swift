a5950961847360f19e5ad529c29585940a72fb86
isTheo <matteohg07@gmail.com>
2024-06-02 19:33:49 +0200

    project27 updated

--- Project27-CoreGraphics/ViewController.swift
+++ Project27-CoreGraphics/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    var currentDrawType = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        drawRectangle()
     }
+    
+    
+    
+    @IBAction func redrawTapped(_ sender: UIButton) {
+        currentDrawType += 1
 
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
 
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+
+        default:
+            break
+        }
+    }
+    
+    
+    
+    func drawRectangle() {
+
+    }
+    
+    
+    
 }
 
